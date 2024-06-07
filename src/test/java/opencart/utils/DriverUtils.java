991b31a1cd2e449ed39c79d9ffa72acf61fe98d6
PAVEL PASHCHENKOV <142984080+ppashchenkov@users.noreply.github.com>
2024-06-07 21:07:16 +0300

    Edit NavigationTest (4 menu's) (#5)

--- src/test/java/opencart/utils/DriverUtils.java
+++ src/test/java/opencart/utils/DriverUtils.java
@@ -1,4 +1,4 @@
-package com.lumatest.utils;
+package opencart.utils;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -19,35 +19,34 @@ public class DriverUtils {
         chromeOptions.addArguments("--no-sandbox");
         chromeOptions.addArguments("--disable-dev-shm-usage");
         chromeOptions.addArguments("--disable-web-security");
-        chromeOptions.addArguments("--allow-running-insecure-content");
-        chromeOptions.addArguments("--ignore-certificate-errors");
+//        chromeOptions.addArguments("--allow-running-insecure-content");
+//        chromeOptions.addArguments("--ignore-certificate-errors");
     }
 
 //for Luma
-    public static WebDriver createChromeDriver(WebDriver driver) {
-        if (driver != null) {
-            driver.quit();
-        }
-
-        ChromeDriver chromeDriver = new ChromeDriver(chromeOptions);
-        chromeDriver.executeCdpCommand("Network.enable", Map.of());
-        chromeDriver.executeCdpCommand(
-                "Network.setExtraHTTPHeaders", Map.of("headers", Map.of("accept-language", "en-US,en;q=0.9"))
-        );
-
-
-        return chromeDriver ;
-    }
+//    public static WebDriver createChromeDriver(WebDriver driver) {
+//        if (driver != null) {
+//            driver.quit();
+//        }
+//
+//        ChromeDriver chromeDriver = new ChromeDriver(chromeOptions);
+//        chromeDriver.executeCdpCommand("Network.enable", Map.of());
+//        chromeDriver.executeCdpCommand(
+//                "Network.setExtraHTTPHeaders", Map.of("headers", Map.of("accept-language", "en-US,en;q=0.9"))
+//        );
+//
+//        return chromeDriver ;
+//    }
 
 // For Open Cart
-//    public static WebDriver createChromeDriver(WebDriver driver) {
-//        if (driver == null) {
-//             return new ChromeDriver(chromeOptions);
-//        } else {
-//            driver.quit();
-//            return new ChromeDriver(chromeOptions);
-//        }
-//    }
+    public static WebDriver createChromeDriver(WebDriver driver) {
+        if (driver == null) {
+             return new ChromeDriver(chromeOptions);
+        } else {
+            driver.quit();
+            return new ChromeDriver(chromeOptions);
+        }
+    }
 
 
 
