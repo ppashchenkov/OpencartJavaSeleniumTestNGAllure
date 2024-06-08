f318f667c5a6d3cbd8d4159c5a323c62a9e6fa7c
ppashchenkov <ppashchenkov@gmail.com>
2024-06-08 21:03:36 +0300

    Add testng.xml

--- src/test/java/opencart/base/BaseTest.java
+++ src/test/java/opencart/base/BaseTest.java
@@ -3,28 +3,49 @@ package opencart.base;
 import opencart.utils.DriverUtils;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
+import org.testng.Reporter;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Parameters;
 
 public abstract class BaseTest {
     private WebDriver driver;
-
+//    private final String browser = "chrome";
+//
     @BeforeSuite
     protected void setupWebDriverManager() {
         WebDriverManager.chromedriver().setup();
+        WebDriverManager.firefoxdriver().setup();
+//        WebDriverManager.safaridriver().setup();
+        //    WebDriverManager.edgedriver().setup();
+        //    WebDriverManager.operadriver().setup();
+        //    WebDriverManager.chromiumdriver().setup()
+        //    WebDriverManager.iedriver().setup();
     }
 
+
+    @Parameters("browser")
     @BeforeMethod
-    protected void setupDriver() {
-        this.driver = DriverUtils.createChromeDriver(getDriver());
+    protected void setupDriver(String browser) {
+        Reporter.log("______________________________________________________", true);
+        this.driver = DriverUtils.createDriver(browser, this.driver);
+        if(getDriver() == null) {
+            Reporter.log("ERROR: Unknown parameter 'browser' = '" + browser + "'", true);
+            System.exit(1);
+        }
+        Reporter.log("INFO: " + browser.toUpperCase() + " driver created", true);
     }
 
+    @Parameters("browser")
     @AfterMethod(alwaysRun = true)
-    protected void tearDown() {
+    protected void tearDown(String browser) {
         if (this.driver != null) {
             getDriver().quit();
+            Reporter.log("INFO: " + browser.toUpperCase() + " driver closed", true);
             this.driver = null;
+        } else {
+            Reporter.log("INFO: Driver is null", true);
         }
     }
 
@@ -32,8 +53,4 @@ public abstract class BaseTest {
 
         return this.driver;
     }
-
-
-
-
 }
--- src/test/java/opencart/utils/DriverUtils.java
+++ src/test/java/opencart/utils/DriverUtils.java
@@ -3,15 +3,18 @@ package opencart.utils;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-
-import java.util.Map;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.safari.SafariOptions;
 
 public class DriverUtils {
     private static final ChromeOptions chromeOptions;
+    private static final FirefoxOptions firefoxOptions;
+    private static final SafariOptions safariOptions;
 
     static {
         chromeOptions = new ChromeOptions();
-
         chromeOptions.addArguments("--incognito");
         chromeOptions.addArguments("--headless");
         chromeOptions.addArguments("--window-size=1920,1080");
@@ -19,27 +22,28 @@ public class DriverUtils {
         chromeOptions.addArguments("--no-sandbox");
         chromeOptions.addArguments("--disable-dev-shm-usage");
         chromeOptions.addArguments("--disable-web-security");
-//        chromeOptions.addArguments("--allow-running-insecure-content");
-//        chromeOptions.addArguments("--ignore-certificate-errors");
+
+        firefoxOptions = new FirefoxOptions();
+        firefoxOptions.addArguments("--incognito");
+        firefoxOptions.addArguments("--headless");
+        firefoxOptions.addArguments("--window-size=1920,1080");
+        firefoxOptions.addArguments("--disable-gpu");
+        firefoxOptions.addArguments("--no-sandbox");
+        firefoxOptions.addArguments("--disable-dev-shm-usage");
+        firefoxOptions.addArguments("--disable-web-security");
+
+        safariOptions = new SafariOptions();
+//        safariOptions.("--incognito");
+//        safariOptions.addArguments("--headless");
+//        safariOptions.addArguments("--window-size=1920,1080");
+//        safariOptions.addArguments("--disable-gpu");
+//        safariOptions.addArguments("--no-sandbox");
+//        safariOptions.addArguments("--disable-dev-shm-usage");
+//        safariOptions.addArguments("--disable-web-security");
+
     }
 
-//for Luma
-//    public static WebDriver createChromeDriver(WebDriver driver) {
-//        if (driver != null) {
-//            driver.quit();
-//        }
-//
-//        ChromeDriver chromeDriver = new ChromeDriver(chromeOptions);
-//        chromeDriver.executeCdpCommand("Network.enable", Map.of());
-//        chromeDriver.executeCdpCommand(
-//                "Network.setExtraHTTPHeaders", Map.of("headers", Map.of("accept-language", "en-US,en;q=0.9"))
-//        );
-//
-//        return chromeDriver ;
-//    }
-
-// For Open Cart
-    public static WebDriver createChromeDriver(WebDriver driver) {
+    private static WebDriver createChromeDriver(WebDriver driver) {
         if (driver == null) {
              return new ChromeDriver(chromeOptions);
         } else {
@@ -48,11 +52,38 @@ public class DriverUtils {
         }
     }
 
+    private static WebDriver createFirefoxDriver(WebDriver driver) {
+        if (driver == null) {
+            return new FirefoxDriver(firefoxOptions);
+        } else {
+            driver.quit();
+            return new FirefoxDriver(firefoxOptions);
+        }
+    }
 
+    private static WebDriver createSafariDriver(WebDriver driver) {
+        if (driver == null) {
+            return new SafariDriver();
+        } else {
+            driver.quit();
+            return new SafariDriver();
+        }
+    }
 
-//    private void createFireFoxDriver() {
-//        if (this.driver == null) {
-//            this.driver = new FirefoxDriver();
-//        }
-//    }
+    public static WebDriver createDriver(String browser, WebDriver driver) {
+        switch (browser) {
+            case "chrome" -> {
+                return createChromeDriver(driver);
+            }
+            case "firefox" -> {
+                return createFirefoxDriver(driver);
+            }
+            case "safari" -> {
+                return createSafariDriver(driver);
+            }
+            default -> {
+                return null;
+            }
+        }
+    }
 }
